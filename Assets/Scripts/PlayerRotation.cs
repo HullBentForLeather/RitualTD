14cc1258e776b6ed6dc8cbe201392e7c41c8319b
unknown <root@CO45787.net.dcs.hull.ac.uk>
2016-01-30 13:33:43 +0000

    new control thingy

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class PlayerRotation : MonoBehaviour {
 
     
-    public Vector2 rightThumbStick;
+    public Vector2 playerFacingPosition;
     public Vector3 direction;
     float heading = 0;
+    Vector3 newRotation;
 
     // Use this for initialization
     void Start()
@@ -17,21 +18,42 @@ public class PlayerRotation : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
-       
-        rightThumbStick = new Vector2(Input.GetAxis("Right Horizontal"), Input.GetAxis("Right Vertical"));
+        float speed = Input.GetAxis("Fire2");
+        float speedRight = Input.GetAxis("Fire1");
+
+        if (speed > 0.1f)
+        {
+          Quaternion newRotation = Quaternion.Euler(0, - speed, 0);
+            
+            transform.rotation = transform.rotation * newRotation;
+
+        }
+        if (speedRight > 0.1f)
+        {
+            Quaternion newRotation = Quaternion.Euler(0, speedRight, 0);
+
+            transform.rotation = transform.rotation * newRotation;
+        }
+
+      //playerFacingPosition = new Vector2(Input.GetAxis("Fire1"), Input.GetAxis("Fire1"));
         
+       
 
-        Transform mainCamera = Camera.main.transform;
-        direction = new Vector3(rightThumbStick.x, 0, rightThumbStick.y);
+        //Transform mainCamera = Camera.main.transform;
+        //direction = new Vector3(playerFacingPosition.x, 0, playerFacingPosition.y);
 
-        if (rightThumbStick.sqrMagnitude < 0.1f)
-        {
+        //if (playerFacingPosition.sqrMagnitude < 0.1f)
+        //{
 
-        }
-        else
-        {
-            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-        }
+        //}
+        //else
+        //{
+        //    // transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+
+      
+        //}
+
+
         //heading = Mathf.Atan2(rightThumbStick.x, rightThumbStick.y) * Mathf.Rad2Deg;
         //transform.rotation =  Quaternion.Euler(0f, heading * Mathf.Rad2Deg, 0f );
 
