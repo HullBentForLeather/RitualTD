05f3f7a51abf25813c89dca9fea80faf4429a241
Sam Spain <sam@samspain.com>
2016-01-30 14:15:33 +0000

    Fixed rotation controls. Broken shooting controls.

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -1,59 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerRotation : MonoBehaviour {
+public class PlayerRotation : MonoBehaviour
+{
 
-    
-    public Vector2 playerFacingPosition;
+    public GameObject bullet;
+    float shootDelay = 0.1f;
+    bool canShoot;
+
+
+    public Vector2 rightThumbStick;
     public Vector3 direction;
     float heading = 0;
-    Vector3 newRotation;
 
     // Use this for initialization
     void Start()
     {
-        
+        canShoot = true;
+    }
+
+    void ResetShot()
+    {
+        canShoot = true;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float speed = Input.GetAxis("Fire2");
-        float speedRight = Input.GetAxis("Fire1");
 
-        if (speed > 0.1f)
+        rightThumbStick = new Vector2(Input.GetAxis("Right Horizontal"), Input.GetAxis("Right Vertical"));
+
+
+        Transform mainCamera = Camera.main.transform;
+        direction = new Vector3(rightThumbStick.x, 0, rightThumbStick.y);
+
+        if (rightThumbStick.sqrMagnitude < 0.1f)
         {
-          Quaternion newRotation = Quaternion.Euler(0, - speed, 0);
-            
-            transform.rotation = transform.rotation * newRotation;
 
         }
-        if (speedRight > 0.1f)
+        else
         {
-            Quaternion newRotation = Quaternion.Euler(0, speedRight, 0);
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
 
-            transform.rotation = transform.rotation * newRotation;
+            if (canShoot)
+            {
+                Instantiate(bullet, transform.position, transform.rotation);
+
+                canShoot = false;
+                Invoke("ResetShot", shootDelay);
+            }
         }
-
-      //playerFacingPosition = new Vector2(Input.GetAxis("Fire1"), Input.GetAxis("Fire1"));
-        
-       
-
-        //Transform mainCamera = Camera.main.transform;
-        //direction = new Vector3(playerFacingPosition.x, 0, playerFacingPosition.y);
-
-        //if (playerFacingPosition.sqrMagnitude < 0.1f)
-        //{
-
-        //}
-        //else
-        //{
-        //    // transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-
-      
-        //}
-
-
         //heading = Mathf.Atan2(rightThumbStick.x, rightThumbStick.y) * Mathf.Rad2Deg;
         //transform.rotation =  Quaternion.Euler(0f, heading * Mathf.Rad2Deg, 0f );
 
