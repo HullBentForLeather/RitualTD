5ef2dd01d95c9a0feed32bd181b8545de9af0fa6
Nick Ross <theincrediblenick@hotmail.co.uk>
2016-01-30 03:18:47 +0000

    Added simple particles that spin around an empty game object and then start spinning outwards

---
+++ Assets/Scripts/ExpandControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExpandControl : MonoBehaviour {
+
+    public int RateOfExpansion;
+    public float SpeedOfExpansion;
+
+    private float frameCounter;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        frameCounter += Time.deltaTime;
+        if (frameCounter > RateOfExpansion)
+        {
+            Vector3 local = transform.localPosition;
+            local.x += SpeedOfExpansion;
+            transform.localPosition = local;
+            frameCounter = 0f;
+        }
+	}
+}
---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    public float RotationSpeedX;
+    public float RotationSpeedY;
+    public float RotationSpeedZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(RotationSpeedX * Time.deltaTime, RotationSpeedY * Time.deltaTime, RotationSpeedZ * Time.deltaTime));
+    }
+}
